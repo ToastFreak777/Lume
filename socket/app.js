39e5a26ff457bd41668ecbb40e68e3c084b110f2
ToastFreak777 <gmcdonald604@gmail.com>
2025-02-26 03:12:31 -0500

    Add connection logs

--- socket/app.js
+++ socket/app.js
@@ -6,8 +6,32 @@ const io = new Server({
   },
 });
 
+let connections = {
+  client: "disconnected",
+  server: "disconnected",
+};
+
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  const { origin } = socket.handshake.headers;
+
+  if (origin.includes("5173")) {
+    connections.client = "connected";
+  } else if (origin.includes("8080")) {
+    connections.server = "connected";
+  }
+  console.log(connections);
+  // console.log(socket.handshake.headers);
+});
+
+io.on("disconnect", () => {
+  const { origin } = socket.handshake.headers;
+
+  if (origin.includes("5173")) {
+    connections.client = "disconnected";
+  } else if (origin.includes("8080")) {
+    connections.server = "disconnected";
+  }
+  console.log(connections);
 });
 
 io.listen("4000");
