6750e45e4f7993c4796cfbda0e6f6c51d0f51ded
ToastFreak777 <gmcdonald604@gmail.com>
2025-02-26 03:40:31 -0500

    Move socket into backend

---
+++ backend/socket.js
@@ -0,0 +1,34 @@
+import { Server } from "socket.io";
+
+export const setUpSocket = () => {
+  // const server = createServer(app);
+  // const io = new Server(server);
+  const io = new Server({
+    cors: {
+      origin: process.env.CLIENT_URL,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    // const { origin } = socket.handshake.headers;
+
+    // if (origin.includes(process.env.CLIENT_URL)) {
+    // }
+    log("client connected");
+    // console.log(socket.handshake.headers);
+  });
+
+  io.on("disconnect", () => {
+    // const { origin } = socket.handshake.headers;
+
+    // if (origin.includes(process.env.CLIENT_URL)) {
+    // }
+    log("client disconnected");
+  });
+
+  io.listen(process.env.SOCKET_PORT);
+};
+
+const log = (message) => {
+  console.log(`Socket: ${message}`);
+};
