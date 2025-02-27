a45ba20b4337e7296cb341a3c8917cc4c8e052e6
ToastFreak777 <gmcdonald604@gmail.com>
2025-02-27 01:53:55 -0500

    Add CRUD stub attendance

--- backend/controllers/attendance.controller.js
+++ backend/controllers/attendance.controller.js
@@ -1,19 +1,104 @@
+import { StatusCodes } from "http-status-codes";
+import Attendance from "../models/attendance.model.js";
+import Users from "../models/users.model.js";
+import { validateFields } from "../utils/helpers.js";
+import { createCustomError } from "../errors/custom-error.js";
+
 export const getAllAttendance = async (req, res) => {
-  res.send("getAllAttendance");
+  const attendance = await Attendance.find({});
+
+  res.status(StatusCodes.OK).json(attendance);
+};
+export const getAttendanceForCourse = async (req, res) => {
+  const attendance = await Attendance.find({ course: req.body.course });
+
+  res.status(StatusCodes.OK).json(attendance);
 };
 
 export const getAttendance = async (req, res) => {
-  res.send("getAttendance");
+  const attendance = await Attendance.find({ _id: req.params.id });
+
+  res.status(StatusCodes.OK).json(attendance);
 };
 
-export const addAttendance = async (req, res) => {
-  res.send("addAttendance");
+export const addAttendance = async (req, res, next) => {
+  const requiredFields = {
+    name: "Attendance Name",
+    course: "Course Id",
+    dueDate: "Due Date",
+    maxGrade: "Max Grade",
+  };
+
+  if (
+    !validateFields(
+      requiredFields,
+      req.body,
+      next,
+      createCustomError,
+      StatusCodes.BAD_REQUEST
+    )
+  )
+    return;
+
+  const user = await Users.findById(req.user.id).lean();
+
+  const notFaculty = !["Admin", "Instructor"].includes(user.role);
+
+  if (notFaculty)
+    return next(
+      createCustomError(
+        "You're not authorized to perform this action",
+        StatusCodes.BAD_REQUEST
+      )
+    );
+
+  const attendance = await Attendance.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: `${attendance.name} created` });
 };
 
 export const updateAttendance = async (req, res) => {
-  res.send("updateAttendance");
+  const user = await Users.findById(req.user.id).lean();
+
+  const notFaculty = !["Admin", "Instructor"].includes(user.role);
+
+  if (notFaculty)
+    return next(
+      createCustomError(
+        "You're not authorized to perform this action",
+        StatusCodes.BAD_REQUEST
+      )
+    );
+
+  const attendance = await Attendances.findByIdAndUpdate(
+    req.params.id,
+    {
+      ...req.body,
+    },
+    { new: true }
+  );
+
+  res.status(StatusCodes.OK).json({
+    msg: `${attendance.name} attendance updated successfully`,
+    attendance,
+  });
 };
 
 export const deleteAttendance = async (req, res) => {
-  res.send("deleteAttendance");
+  const user = await Users.findById(req.user.id).lean();
+
+  const notFaculty = !["Admin", "Instructor"].includes(user.role);
+
+  if (notFaculty)
+    return next(
+      createCustomError(
+        "You're not authorized to perform this action",
+        StatusCodes.BAD_REQUEST
+      )
+    );
+
+  const attendance = await Attendance.findByIdAndDelete(req.params.id);
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: `${attendance.name} attendance deleted successfully` });
 };
