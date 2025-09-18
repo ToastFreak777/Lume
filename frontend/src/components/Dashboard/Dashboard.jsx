c172a6e4c8ed275729168396de07577c07931231
ToastFreak <gmcdonald604@gmail.com>
2025-09-17 20:44:12 -0400

    Edit: limit dashboard courses = 4

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,24 +1,25 @@
 import styles from "./Dashboard.module.css";
 
-import {Card} from "../index";
+import { Card } from "../index";
 
-const Dashboard = ({courses}) => {
-
-    return (
-        <main className={styles.dashboard}>
-            <div className={styles.content}>
-                <div className={styles.classes}>
-                    {courses.length > 0 ? (
-                        courses.map((course) => (
-                            <Card key={course._id} card={course} className={styles.class}/>
-                        ))
-                    ) : (
-                        <h2>No Enrolled Courses...</h2>
-                    )}
-                </div>
-            </div>
-        </main>
-    );
+const Dashboard = ({ courses }) => {
+  return (
+    <main className={styles.dashboard}>
+      <div className={styles.content}>
+        <div className={styles.classes}>
+          {courses.length > 0 ? (
+            courses
+              .slice(0, 4)
+              .map((course) => (
+                <Card key={course._id} card={course} className={styles.class} />
+              ))
+          ) : (
+            <h2>No Enrolled Courses...</h2>
+          )}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Dashboard;
